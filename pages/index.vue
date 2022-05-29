2dfac4e1f45320d544f83eb2df5870ff2d01ac0e
baishiaihui <baishiaihui@gmail.com>
2022-05-30 00:29:44 +0900

    Create Search

--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,75 @@
 <template>
   <div>
-    <h2>
-      川崎市　ゴミ分別情報
-    </h2>
+    <h2>川崎市　ゴミ分別情報</h2>
+
+    <div class="input-group mb-3">
+      <input
+        type="text"
+        class="form-control"
+        width="500"
+        placeholder="検索キーワードを入力してください"
+        v-model="keyword"
+      />
+      <button
+        class="btn btn-success"
+        type="button"
+        @click="search"
+      >
+        検索
+      </button>
+    </div>
+
     <table v-if="sortinfos.length">
       <thead>
         <tr>
-          <th>name</th>
-          <th>kana</th>
-          <th>sort</th>
+          <th>品目名</th>
+          <th>分別タイプ</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="info in sortinfos" :key="info.id">
           <td>{{ info.name }}</td>
-          <td>{{ info.kana }}</td>
           <td>{{ info.sort }}</td>
         </tr>
       </tbody>
     </table>
 
-    <div v-else>
-      情報が取得できませんでした
-    </div>
+    <div v-else>{{ message }}</div>
   </div>
 </template>
 
 <script>
 export default {
-  async asyncData ({ $axios }) {
-    let sortinfos = []
-    await $axios.$get('/api/v1/sort_infos')
-      .then(res => (sortinfos = res))
-    return { sortinfos }
-  }
-}
+  data() {
+    return {
+      message: "ここに検索結果が表示されます",
+      keyword: "",
+      sortinfos: [],
+    };
+  },
+  methods: {
+    search() {
+      if (this.keyword == "") {
+        alert("検索キーワードを入力してください！");
+      } else {
+        const url = "/api/v1/search";
+        this.$axios
+          .get(url, { params: { keyword: this.keyword } })
+          .then((res) => {
+            this.sortinfos = res.data.sortinfos;
+
+            if (this.sortinfos.length) {
+              //何もしない
+            } else {
+              this.message =
+                "入力されたキーワードを含む品目は見つかりませんでした。";
+            }
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+    },
+  },
+};
 </script>
